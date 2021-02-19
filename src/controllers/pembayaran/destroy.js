e954e32526be242f35c19030e6f26c146bd627eb
dickymahfudin <dickymahfudin13@gmail.com>
2021-02-19 22:27:11 +0700

    api destroy pembayaran

--- src/controllers/pembayaran/destroy.js
+++ src/controllers/pembayaran/destroy.js
@@ -3,13 +3,6 @@ const { pembayaran } = require("../../models");
 module.exports = async (req, res) => {
   const { id } = req.params;
 
-  if (!id) {
-    return res.status(409).json({
-      status: "error",
-      data: "masukkan Parameter",
-    });
-  }
-
   const destroyPembayaran = await pembayaran.destroy({ where: { id } });
   return res.status(200).json({
     status: "success",
