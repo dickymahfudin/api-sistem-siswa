19f16488678ef18ac22048444784cd7274b4310e
dickymahfudin <dickymahfudin13@gmail.com>
2021-02-19 22:27:27 +0700

    relasi

--- src/models/index.js
+++ src/models/index.js
@@ -31,7 +31,6 @@ fs.readdirSync(__dirname)
       sequelize,
       Sequelize.DataTypes
     );
-    console.log(model);
     db[model.name] = model;
   });
 
--- src/models/pembayaran.js
+++ src/models/pembayaran.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.user, {
+        foreignKey: "user_id",
+        as: "user",
+      });
     }
   }
   pembayaran.init(
