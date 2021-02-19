aea1682a0bbd5e629aa102e78c114dc6e3bd6b8d
dickymahfudin <dickymahfudin13@gmail.com>
2021-02-19 22:20:12 +0700

    Get User

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,4 +8,5 @@ router.post("/register", usersController.register);
 router.post("/login", usersController.login);
 router.put("/:id", middleware, usersController.update);
 router.delete("/:id", middleware, usersController.destroy);
+router.get("/", middleware, usersController.get);
 module.exports = router;
